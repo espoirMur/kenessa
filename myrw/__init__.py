0cce49eb68ad911c1603ed1087bd62c9d866115d
Remy Muhire <rmuhire@exuus.com>
2016-11-17 15:46:08 +0200

    json output province

--- myrw/__init__.py
+++ myrw/__init__.py
@@ -1,6 +1,10 @@
 import json
+import os
+import myrw
 
-
+path = os.path.dirname(myrw.__file__) + '/json/province.json'
 
 def hello():
-    return json.dumps({'Message':'Hello! This is myrw'})
+    with open(path) as json_data:
+        data = json.load(json_data)
+    return json.dumps({'Data': data})
