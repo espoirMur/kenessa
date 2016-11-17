d3da9a2c2767f0163f5af89f385117ba56c1b637
Remy Muhire <rmuhire@exuus.com>
2016-11-17 14:19:12 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='myrw',
+      version='0.1',
+      description='Rwanda Administrative Location',
+      url='https://github.com/rmuhire/myrw',
+      author='Remy Muhire, Dieume Hirwa',
+      author_email='rmuhire@exuus.com, dhirwa@exuus.com',
+      license='MIT',
+      packages=['myrw'],
+      zip_safe=False)
