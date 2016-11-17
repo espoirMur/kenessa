f890318a5285b56341faadb40c3871d35b96802c
Remy Muhire <rmuhire@exuus.com>
2016-11-17 17:38:01 +0200

    Simple Detection Algo

--- myrw/__init__.py
+++ myrw/__init__.py
@@ -3,10 +3,29 @@ import os
 import myrw
 
 
-path = os.path.dirname(myrw.__file__) + '/json/province.json'
+json_province = os.path.dirname(myrw.__file__) + '/json/province.json'
+json_district = os.path.dirname(myrw.__file__) + '/json/district.json'
+json_sector = os.path.dirname(myrw.__file__) + '/json/sector.json'
 
-def hello():
-    with open(path) as json_data:
-        data = json.load(json_data)
-    return json.dumps({'Data': data})
+class Province:
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.params = ''
+
+        if isinstance(identifier, str):
+            if self.identifier[0] == '0':
+                self.params = 'code'
+            else:
+                try:
+                    int(self.identifier)
+                    self.params = 'id'
+                except ValueError:
+                    self.params = 'Name'
+        else:
+            self.params = 'Please define a string'
+
+    def province(self):
+
+        return self.params
+
 
