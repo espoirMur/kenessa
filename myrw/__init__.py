8aac632e2a67f706c11e926fd96b631d1f66eb9e
Remy Muhire <rmuhire@exuus.com>
2016-11-22 15:21:49 +0200

    Province class

--- myrw/__init__.py
+++ myrw/__init__.py
@@ -12,23 +12,30 @@ class Province:
         self.json_district =  os.path.dirname(myrw.__file__) + '/json/district.json'
         self.json_sector =  os.path.dirname(myrw.__file__) + '/json/sector.json'
 
-        if self.identifier.lower() == 'all':
-            self.params = 'all'
+        try:
+            if self.identifier.lower() == 'all':
+                self.params = 'all'
 
-        elif isinstance(identifier, str):
-            if self.identifier[0] == '0':
-                self.params = 'code'
-            else:
-                try:
-                    self.identifier = int(self.identifier)
-                    self.params = 'id'
-                except ValueError:
-                    self.params = 'name'
-        else:
-            self.params = 'Please define a string'
+            if isinstance(identifier, str):
+                if self.identifier[0] == '0':
+                    self.params = 'code'
+                else:
+                    try:
+                        self.identifier = int(self.identifier)
+                        self.params = 'id'
+                    except ValueError:
+                        self.params = 'name'
+            self.status = 'success'
+        except AttributeError:
+            self.status = 'error'
+
 
     def province(self):
         json_province = json.loads(open(self.json_province).read())
+
+        if self.status == 'error':
+            return json.dumps({'error': 'class Province allow string not int'})
+
 
         if self.params == 'all':
             return json.dumps({'provinces': json_province},
@@ -45,13 +52,25 @@ class Province:
                 except AttributeError:
                     if province[self.params] == self.identifier:
                         json_data['province'].append(province)
-            return json.dumps({json_data},
+            return json.dumps(json_data,
                         sort_keys=True,
                         indent=4,
                         separators=(',', ': ')
                     )
 
 
+    def district(self):
+        json_province =  json.loads(open(self.json_province).read())
+        json_district = json.loads(open(self.json_district).read())
+        json_data = {}
+
+        if self.status == 'error':
+            return json.dumps({'error': 'class Province allow string not int'})
+
+        if self.params == 'all':
+            for province in json_province:
+                for district in json_district:
+                    pass
 
 
 
