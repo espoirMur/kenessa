fe608ee0398349c86b5bc04cfc903d0503b6f315
Remy Muhire <rmuhire@exuus.com>
2016-11-22 19:27:25 +0200

    district class done

--- myrw/__init__.py
+++ myrw/__init__.py
@@ -16,7 +16,7 @@ class Province:
             if self.identifier.lower() == 'all':
                 self.params = 'all'
 
-            if isinstance(identifier, str):
+            elif isinstance(identifier, str):
                 if self.identifier[0] == '0':
                     self.params = 'code'
                 else:
@@ -30,6 +30,7 @@ class Province:
             self.status = 'error'
 
 
+
     def province(self):
         json_province = json.loads(open(self.json_province).read())
 
@@ -40,7 +41,7 @@ class Province:
         if self.params == 'all':
             return json.dumps({'provinces': json_province},
                         sort_keys=True,
-                        indent=4,
+                        indent=3,
                         separators=(',', ': ')
                     )
         else:
@@ -54,7 +55,7 @@ class Province:
                         json_data['province'].append(province)
             return json.dumps(json_data,
                         sort_keys=True,
-                        indent=4,
+                        indent=3,
                         separators=(',', ': ')
                     )
 
@@ -62,15 +63,57 @@ class Province:
     def district(self):
         json_province =  json.loads(open(self.json_province).read())
         json_district = json.loads(open(self.json_district).read())
-        json_data = {}
+        data = []
 
         if self.status == 'error':
             return json.dumps({'error': 'class Province allow string not int'})
 
         if self.params == 'all':
             for province in json_province:
+                json_p = {province['name']:[]}
+                province['district'] = ''
+                json_d = []
                 for district in json_district:
-                    pass
+                    if province['id'] == district['province_id']:
+                        json_d.append(district)
+
+                province['district'] = json_d
+                json_p[province['name']].append(province)
+
+                data.append(json_p)
+
+            return json.dumps(data,
+                    sort_keys=False,
+                    indent=3,
+                    separators=(',', ': ')
+                )
+        else:
+            data = {}
+            province = json.loads(Province(str(self.identifier)).province())['province']
+            for item in province:
+                data['id'] = item['id']
+                data['name'] = item['name']
+                data['code'] = item['code']
+
+            json_p = {data['name']:[]}
+            json_d = []
+
+
+            for district in json_district:
+                if data['id'] == district['province_id']:
+                    json_d.append(district)
+
+            data['district'] = json_d
+            json_p[data['name']].append(data)
+
+            return json.dumps(json_p,
+                    sort_keys=False,
+                    indent=3,
+                    separators=(',', ': ')
+                )
+
+
+
 
 
 
