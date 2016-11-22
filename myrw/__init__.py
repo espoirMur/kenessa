c8b185e51e7c35b8da8c956fa996c7f8f081be24
Remy Muhire <rmuhire@exuus.com>
2016-11-22 13:11:31 +0200

    Province class working

--- myrw/__init__.py
+++ myrw/__init__.py
@@ -3,29 +3,55 @@ import os
 import myrw
 
 
-json_province = os.path.dirname(myrw.__file__) + '/json/province.json'
-json_district = os.path.dirname(myrw.__file__) + '/json/district.json'
-json_sector = os.path.dirname(myrw.__file__) + '/json/sector.json'
 
 class Province:
     def __init__(self, identifier):
         self.identifier = identifier
         self.params = ''
+        self.json_province  = os.path.dirname(myrw.__file__) + '/json/province.json'
+        self.json_district =  os.path.dirname(myrw.__file__) + '/json/district.json'
+        self.json_sector =  os.path.dirname(myrw.__file__) + '/json/sector.json'
 
-        if isinstance(identifier, str):
+        if self.identifier.lower() == 'all':
+            self.params = 'all'
+
+        elif isinstance(identifier, str):
             if self.identifier[0] == '0':
                 self.params = 'code'
             else:
                 try:
-                    int(self.identifier)
+                    self.identifier = int(self.identifier)
                     self.params = 'id'
                 except ValueError:
-                    self.params = 'Name'
+                    self.params = 'name'
         else:
             self.params = 'Please define a string'
 
     def province(self):
+        json_province = json.loads(open(self.json_province).read())
 
-        return self.params
+        if self.params == 'all':
+            return json.dumps({'provinces': json_province},
+                        sort_keys=True,
+                        indent=4,
+                        separators=(',', ': ')
+                    )
+        else:
+            json_data = {'province':[]}
+            for province in json_province:
+                try:
+                    if province[self.params].lower() == self.identifier.lower():
+                        json_data['province'].append(province)
+                except AttributeError:
+                    if province[self.params] == self.identifier:
+                        json_data['province'].append(province)
+            return json.dumps({json_data},
+                        sort_keys=True,
+                        indent=4,
+                        separators=(',', ': ')
+                    )
+
+
+
 
 
