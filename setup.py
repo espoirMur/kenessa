d939a33679c33c3b5ea33947a3c991bbaa325864
Remy Muhire <rmuhire@exuus.com>
2016-11-25 12:45:14 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup
 
-setup(name='myrw',
-      version='0.1',
+setup(
+      name='myrw',
+      version='0.1.1',
       description='Rwanda Administrative Location',
       classifiers=[
-            'Development Status :: 3 - Alpha',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Rwanda Administrative Location :: Linguistic',
       ],
       keywords= 'rwanda map administration package',
       url='https://github.com/rmuhire/myrw',
@@ -15,11 +12,12 @@ setup(name='myrw',
       author_email='rmuhire@exuus.com, dhirwa@exuus.com',
       license='MIT',
       packages=['myrw'],
-      install_requires = [
-
-      ],
       entry_points={
           'console_scripts': ['myrw-province=myrw.command_line:main'],
       },
-      include_package_data=True,
-      zip_safe=False)
+      package_data = {'myrw': ['json/*.json']},
+      zip_safe=False
+)
+
+
+
