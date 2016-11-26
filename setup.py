38e7f68cea505ba58fb4b92f486688fbc2177883
Remy Muhire <rmuhire@exuus.com>
2016-11-26 17:37:33 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
       name='kenessa',
-      version='0.1.4',
+      version='0.1.5',
       description='Rwanda Administrative Location',
       classifiers=[
       ],
-      keywords= 'rwanda map administration package',
+      keywords= 'rwanda administrative location',
       url='https://github.com/rmuhire/kenessa',
       author='Remy Muhire',
       author_email='rmuhire@exuus.com',
