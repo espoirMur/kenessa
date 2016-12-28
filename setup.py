befc682a18b8f4af2c18a64534385b03207cfae7
rmuhire <rmuhire@exuus.com>
2016-12-29 00:12:35 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='kenessa',
-      version='0.1.75',
+      version='0.1.76',
       description='Rwanda Administrative Location',
       classifiers=[
       ],
