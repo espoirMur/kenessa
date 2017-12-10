c0c0da41e7c460eec175fda35eb1194a2123234d
remymuhire <jeanremy1806@gmail.com>
2017-12-10 12:19:23 +0200

    bugs fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='kenessa',
-      version='0.1.86',
+      version='0.1.9',
       description='Rwanda Administrative Location',
       classifiers=[
             "Programming Language :: Python",
