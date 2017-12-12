2b16b4337e99463cc45b77a90a51ab56679d6bed
remymuhire <jeanremy1806@gmail.com>
2017-12-12 10:03:16 +0200

    kenessa

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='kenessa',
-      version='0.1.9',
+      version='0.1.92',
       description='Rwanda Administrative Location',
       classifiers=[
             "Programming Language :: Python",
